c9db4f4af915322c735f1374eb43f9b97a75ea1a
Diogo Biazus <diogob@gmail.com>
2011-12-24 15:05:32 -0200

    we'll need just one describe for now in id-generator_spec

--- spec/id-generator_spec.js
+++ spec/id-generator_spec.js
@@ -1,31 +1,29 @@
 var generate = require("../lib/node-load-balancer/id-generator");
 
 describe("id-generator", function(){
-  describe("generate", function(){
-    var callback = jasmine.createSpy('callback');
-    var ids = null;
+  var callback = jasmine.createSpy('callback');
+  var ids = null;
 
-    beforeEach(function(){
-      ids = [];
-      callback.andCallFake(function(arg1, id){
-        ids.push(id);
-      });
-      generate(callback);
+  beforeEach(function(){
+    ids = [];
+    callback.andCallFake(function(arg1, id){
+      ids.push(id);
     });
+    generate(callback);
+  });
 
-    it("should generate a string", function(){
-      expect(callback).toHaveBeenCalledWith(null, jasmine.any(String));
-    });
+  it("should generate a string", function(){
+    expect(callback).toHaveBeenCalledWith(null, jasmine.any(String));
+  });
 
-    it("should generate different strings in consecutive calls", function(){
-      generate(callback);
-      ids[0]
-      expect(ids[0]).toNotEqual(ids[1]);
-    });
+  it("should generate different strings in consecutive calls", function(){
+    generate(callback);
+    ids[0]
+    expect(ids[0]).toNotEqual(ids[1]);
+  });
 
-    it("should generate the id with length passed in as options", function(){
-      generate({length: 32}, callback);
-      expect(ids[1].length).toEqual(32);
-    });
+  it("should generate the id with length passed in as options", function(){
+    generate({length: 32}, callback);
+    expect(ids[1].length).toEqual(32);
   });
 });
