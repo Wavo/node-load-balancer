5506caf4fab653e5869adcc149f82623b7e4801f
Diogo Biazus <diogob@gmail.com>
2011-12-24 15:57:22 -0200

    calls next middleware layer

--- spec/client-id-manager_spec.js
+++ spec/client-id-manager_spec.js
@@ -1,13 +1,20 @@
 var clientIdManager = require("../lib/node-load-balancer/client-id-manager");
 
 describe("client-id-manager", function(){
+  var middleware = clientIdManager();
+
   it("should return a function", function(){
-    expect(clientIdManager()).toEqual(jasmine.any(Function));
+    expect(middleware).toEqual(jasmine.any(Function));
   });
 
   it("should throw exception from returned function when req does not have cookies", function(){
-    var middleware = clientIdManager();
     expect(function(){ middleware({}, {}, function(){}); }).toThrow(middleware.cookieDecoderError);
+  });
+
+  it("should call next if we have the cookies object on req", function(){
+    var next = jasmine.createSpy('next');
+    middleware({cookies: {}}, {}, next);
+    expect(next).toHaveBeenCalled();
   });
 
 });
