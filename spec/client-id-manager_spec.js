0ddae1e0cdb25595f9954232526907d0b8696757
Diogo Biazus <diogob@gmail.com>
2011-12-27 12:09:02 -0200

    now we generate the cookie with client id if the client does not have one already

--- spec/client-id-manager_spec.js
+++ spec/client-id-manager_spec.js
@@ -2,6 +2,18 @@ var clientIdManager = require("../lib/node-load-balancer/client-id-manager");
 
 describe("client-id-manager", function(){
   var middleware = clientIdManager();
+  var next = jasmine.createSpy('next');
+  var res = {
+    on: function(event, callback){
+      callback();
+    },
+    setHeader: function(){}
+  };
+  var req = {cookies: {}};
+
+  beforeEach(function(){
+    spyOn(res, "on").andCallThrough();
+  });
 
   it("should return a function", function(){
     expect(middleware).toEqual(jasmine.any(Function));
@@ -12,9 +24,24 @@ describe("client-id-manager", function(){
   });
 
   it("should call next if we have the cookies object on req", function(){
-    var next = jasmine.createSpy('next');
-    middleware({cookies: {}}, {}, next);
+    middleware(req, res, next);
     expect(next).toHaveBeenCalled();
+  });
+
+  it("should call setHeader inside head event callback if there is no NLBClientID in cookies", function(){
+    spyOn(res, "setHeader").andCallFake(function(header, value){
+      expect(value).toMatch(/^NLBClientID=\d+;$/);
+    });
+    middleware(req, res, next);
+    expect(res.on).toHaveBeenCalledWith('header', jasmine.any(Function));
+    expect(res.setHeader).toHaveBeenCalledWith('Set-Cookie', jasmine.any(String));
+  });
+
+  it("should not call anything in head event callback if there is NLBClientID already in cookies", function(){
+    // The cookie parser always put names in lowercase
+    req.cookies = {'nlbclientid': '1'};
+    middleware(req, res, next);
+    expect(res.on).wasNotCalled();
   });
 
 });
