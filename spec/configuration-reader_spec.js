660971147e5e2aee2a2c1c2247d807024c762e91
Diogo Biazus <diogob@gmail.com>
2012-01-04 00:07:39 -0200

    adding a configuration reader function

---
+++ spec/configuration-reader_spec.js
@@ -0,0 +1,11 @@
+var configurationReader = require("../lib/node-load-balancer/configuration-reader");
+
+describe("configuration-reader", function(){
+  describe("when we have a valid config file", function(){
+    var config = configurationReader("./spec/fixtures/config.json");
+    it("it should read and load the json in given path", function(){
+      expect(config.routingAlgorithm).toEqual('WeightedLeastConnection');
+      expect(config.precision).toEqual(0.001);
+    });
+  });
+});
