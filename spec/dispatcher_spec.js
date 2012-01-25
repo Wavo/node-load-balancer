b9b0c989f0076b4695be7c5eb1554459e0b2d759
Diogo Biazus <diogob@gmail.com>
2012-01-25 00:19:33 -0200

    basic dispatcher functionality

--- spec/dispatcher_spec.js
+++ spec/dispatcher_spec.js
@@ -11,7 +11,20 @@ describe("dispatcher", function(){
   };
   var req = {cookies: {}};
 
+  beforeEach(function(){
+    spyOn(res, "on").andCallThrough();
+  });
+
   it("should return a function", function(){
     expect(middleware).toEqual(jasmine.any(Function));
+  });
+
+  it("should throw exception from returned function when req does not have cookies", function(){
+    expect(function(){ middleware({}, {}, function(){}); }).toThrow(middleware.cookieDecoderError);
+  });
+
+  it("should call next", function(){
+    middleware(req, res, next);
+    expect(next).toHaveBeenCalled();
   });
 });
